f4898da4433850d4c15483a4ba9004a012f947cf
Kartik Ramesh <kartik.ramesh@icloud.com>
2020-11-11 20:11:32 -0500

    updated comments to query EC2

--- shellscripts/standaloneEC2.sh
+++ shellscripts/standaloneEC2.sh
@@ -3,5 +3,16 @@ aws ec2 run-instances \
 --count 1 \
 --instance-type t2.micro \
 --key-name Test \
---security-group-ids sg-0b372f10ebe311fc4 \
---user-data file://startup_script.sh
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Project,Value=udacity}]' \
+#--security-group-ids sg-0b372f10ebe311fc4 \
+#--user-data file://startup_script.sh 
+
+aws ec2 describe-instances \
+--query 'Reservations[*].Instances[*].[PublicIpAddress,Tags[*]]' \
+--filters "Name=tag:Project,Values=udacity" \
+--output text >> inventory
+
+
+#--query 'Reservations[*].Instances[*].{Instance:InstanceId,AZ:Placement.AvailabilityZone,Name:Tags[?Key==`Name`]|[0].Value}' 
+#aws ec2 describe-instances --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, State.Name, InstanceId]' --output text | grep us-west-2a | grep running | awk '{print $3}'
+#aws ec2 terminate-instances --instance-ids i-1234567890abcdef0
